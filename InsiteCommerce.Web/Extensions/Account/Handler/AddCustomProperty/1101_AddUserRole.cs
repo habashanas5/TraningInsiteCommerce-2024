1cef38912639ef19d001d88af2be1c055de47fba
Anas <Anas@DESKTOP-92O1N2Q>
2024-04-29 19:11:33 +0300

    news

---
+++ InsiteCommerce.Web/Extensions/Account/Handler/AddCustomProperty/1101_AddUserRole.cs
@@ -0,0 +1,29 @@
+﻿using Insite.Account.Services.Parameters;
+using Insite.Account.Services.Results;
+using Insite.Core.Interfaces.Data;
+using Insite.Core.Interfaces.Dependency;
+using Insite.Core.Interfaces.Plugins.Security;
+using Insite.Core.Services.Handlers;
+using Insite.Data.Entities;
+using Insite.Data.Repositories.Interfaces;
+using InsiteCommerce.Web.Extensions.Account.Handler.AddCustomProperty;
+using System.Data.Entity;
+
+
+namespace InsiteCommerce.Web.Extensions.Account.Handler.AddCustomProperty
+{
+    [DependencyName("AddUserRole")]
+    public class AddUserRole: HandlerBase<AddAccountParameter, AddAccountResult>
+    {
+        public AddUserRole() { }
+        public override int Order => 1101;
+        public override AddAccountResult Execute(
+          IUnitOfWork unitOfWork,
+          AddAccountParameter parameter,
+          AddAccountResult result)
+        {
+            result.UserProfile.SetProperty("userRole", "Buyer2");
+            return this.NextHandler.Execute(unitOfWork, parameter, result);
+        }
+    }
+}
